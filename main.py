725e32a9b85d948fdbea484ea12d25ad5e98bda4
Avni Balan <67548289+not-that-python@users.noreply.github.com>
2023-02-25 15:09:38 +0000

    Written an answer to question 1 (not confirmed)

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("main")
+a = int(input())
+b = int(input())
+c = int(input())
+
+phrase = ""
+if (a**2) + (b**2) == c**2:
+  phrase = "equal to"
+elif (a**2) + (b**2) < c**2:
+  phrase = "less than"
+else:
+  phrase = "greater than"
+
+print(f"{a} squared plus {b} squared is {phrase} {c} squared")
