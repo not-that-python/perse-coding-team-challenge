27331de8836afea04d62d5c50f2cb4cd3f5d6526
Avni Balan <67548289+not-that-python@users.noreply.github.com>
2023-03-05 10:06:17 +0000

    Started question 10

---
+++ 2021-22/Round 2/question10.py
@@ -0,0 +1,15 @@
+positions_o = []
+positions_x = []
+for i in range(16):
+  if i%2:
+    positions_x.append(input())
+  else:
+    positions_o.append(input())
+
+x_axis = ['a', 'b', 'c', 'd']
+y_axis = ['1', '2', '3', '4']
+
+score_x = 0
+score_o = 0
+
+
